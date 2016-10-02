c00a28e0d54a178c33e9dc1abe8132664272e050
ZPdesu <1063961494@qq.com>
2016-10-02 15:44:57 +0800

    second modify

--- LUfactorization.py
+++ LUfactorization.py
@@ -3,53 +3,59 @@ from numpy import *
 
 
 def create_matrix():
+    """
+
+    :return: Generate the default matrix A
+    """
     A = mat([[1, 2, -3, 4], [4, 8, 12, -8], [2, 3, 2, 1], [-3, -1, 1, -4]], float)
     return A
 
 
 def factorization(a):
-    # type: (a) -> 输入的矩阵
-    # type: (b) -> 临时排序数组
-    # type: (c) -> 组合数组
-    m, n = shape(a)
+    """
+
+    :param a: Input matrix
+    :return: Decomposed matrices L, U, P
+    """
+    m, n = shape(a)  # Calculate the number of rows and columns of a matrix
     if m != n:
         print "\nThe matrix is not square"
     elif linalg.matrix_rank(a) != n:
         print "\nThis is not a nonsingular matrix"
     else:
-        L = eye(n, dtype=float)
+        L = eye(n, dtype=float)     # Initialize matrix L, U, P
         U = zeros((n, n), float)
         P = zeros((n, n), float)
-        b = arange(1, n + 1)
+        b = arange(1, n + 1, dtype=float)       # b is used to record the order of exchange
         c = c_[a, b]
-        for i in range(0, n - 1):
+        for i in range(0, n - 1):       # Partial Pivoting
             temp = i
-            for j in range(i + 1, n):
+            for j in range(i + 1, n):       # Select and exchange the row that corresponds to  the max absolute value
                 if abs(c[j, i]) > abs(c[temp, i]):
                     temp = j
             temp_row = c[temp].copy()
             c[temp] = c[i]
             c[i] = temp_row
-            for j in range(i + 1, n):
+            for j in range(i + 1, n):       # Eliminate the following lines
                 if abs(c[j, i]) > 0:
                     temp_ratio = c[j, i] / c[i, i]
                     c[j, i: n] = c[j, i: n] - temp_ratio * c[i, i:n]
                     c[j, i] = temp_ratio
-        for i in range(1, n):
+        for i in range(1, n):       # Calculate L
             for j in range(0, i):
                 L[i, j] = c[i, j]
-        for i in range(0, n):
+        for i in range(0, n):       # Calculate U and P
             for j in range(i, n):
                 U[i, j] = c[i, j]
             P[i, int(c[i, n]) - 1] = 1
         print "When A =", '\n', a
-        print "L = ", '\n', L
+        print "Matrix L = ", '\n', L
         print "Matrix U = ", '\n', U
         print "Matrix P = ", '\n', P
         return L, U, P
 
 
-def main():
+def main():     # Test run
     A = create_matrix()
     factorization(A)
 
---
+++ test.py
@@ -0,0 +1,14 @@
+# coding=utf-8
+from numpy import *
+import LUfactorization as lu
+
+
+a = 'yes'
+while a == 'yes':
+    A = input("Enter your input matrix A like \nmatrix([[1, 2, -3, 4], "
+              "[4, 8, 12, -8], [2, 3, 2, 1], [-3, -1, 1, -4]]) \n")
+    L, U, P = lu.factorization(A)
+    print "Matrix L = ", '\n', L
+    print "Matrix U = ", '\n', U
+    print "Matrix P = ", '\n', P
+    a = raw_input("if continue, input yes \n")
