5ab655c40512350ab799af714ee69ac5d134b562
ZPdesu <1063961494@qq.com>
2016-10-06 22:43:22 +0800

    have a try

--- test.py
+++ test.py
@@ -8,6 +8,7 @@ while a == 'yes':
     A = input("Enter your input matrix A like \nmatrix([[1, 2, -3, 4], "
               "[4, 8, 12, -8], [2, 3, 2, 1], [-3, -1, 1, -4]]) \n")
     L, U, P = lu.factorization(A)
+
     print "Matrix L = ", '\n', L
     print "Matrix U = ", '\n', U
     print "Matrix P = ", '\n', P
